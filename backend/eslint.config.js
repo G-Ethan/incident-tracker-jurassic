5d5ec288bccf40fd62e5b8fc506e7c285b7f324e
Ethan GODARD <ethan.godard@edouardgand.fr>
2025-07-05 00:50:37 +0200

    Nouvelle version

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,14 +1,9 @@
-// eslint.config.js
-export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-      // Exemple de règle de base
-      semi: ["error", "always"],
-    },
-  },
-];
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
